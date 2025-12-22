d7813a0bc3cbbef11669aeac9af800caf9def695
RohitBohra-001 <bohrarohit03@gmail.com>
2025-12-22 09:33:44 +0530

    add User model and PostgreSQL integration

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object("config")
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+import models
 
 @app.route("/")
 def home():
---
+++ backend/config.py
@@ -0,0 +1,2 @@
+SQLALCHEMY_DATABASE_URI = "postgresql://mymac@localhost:5433/calendar_db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
