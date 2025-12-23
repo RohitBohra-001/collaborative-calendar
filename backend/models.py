c087b531be8771d9a75cbd98b573ed7a9d34b63d
RohitBohra-001 <bohrarohit03@gmail.com>
2025-12-23 11:41:34 +0530

    add notification model

--- backend/models.py
+++ backend/models.py
@@ -82,3 +82,21 @@ class Availability(db.Model):
         db.DateTime,
         default=lambda: datetime.now(timezone.utc)
     )
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id"),
+        nullable=False
+    )
+
+    message = db.Column(db.String(255), nullable=False)
+
+    is_read = db.Column(db.Boolean, default=False)
+
+    created_at = db.Column(
+        db.DateTime,
+        default=lambda: datetime.now(timezone.utc)
+    )
